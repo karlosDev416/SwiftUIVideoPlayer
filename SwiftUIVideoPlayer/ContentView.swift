019eae01981d89b99a76de831642ef1e4e947ee5
Karlos Aguirre <karlos.dev416@gmail.com>
2023-07-27 23:11:15 -0600

    Add a VideoPlayer view that loads a video from a URL

--- SwiftUIVideoPlayer/ContentView.swift
+++ SwiftUIVideoPlayer/ContentView.swift
@@ -6,16 +6,24 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    
+    let url = URL(string: "https://media1.giphy.com/media/k3yiJHdZrLy0XZ3Qtv/giphy480p.mp4?cid=ecf05e4741u2wxj9q8t837vrntiq3bdpekjj9zsl8mg9thsx&amp;ep=v1_videos_search&amp;rid=giphy480p.mp4&amp;ct=v")!
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VideoPlayer(player: AVPlayer(url: url)) {
+                Text("👍🏼 Si te ha gustado puedes\n darle like al video")
+                    .bold()
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            Text("Aprendiendo SwiftUI - VideoPlayer")
+                .bold()
+                .padding(60)
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
