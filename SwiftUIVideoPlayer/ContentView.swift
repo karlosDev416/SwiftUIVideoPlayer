a54f50e8c895d8e9759a4c68a0062f09ee9f5086
Karlos Aguirre <karlos.dev416@gmail.com>
2023-07-27 22:29:32 -0600

    Initial project

---
+++ SwiftUIVideoPlayer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIVideoPlayer
+//
+//  Created by KARLOS AGUIRRE on 7/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIVideoPlayer/SwiftUIVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVideoPlayerApp.swift
+//  SwiftUIVideoPlayer
+//
+//  Created by KARLOS AGUIRRE on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
